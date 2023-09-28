771fb0c5e18c675dc6abe832ba10ea26ec503c0a
Joris Lamberty <s59@jlamb@uni-bonn.de>
2023-09-28 15:04:40 +0200

    Script for Clicking the Checkboc

---
+++ clicker.js
@@ -0,0 +1,11 @@
+document.getElementById("bahn-bonus-sammeln--checkboxContainer--checkbox").checked = true;
+
+window.addEventListener("click", notifyExtension);
+
+function notifyExtension(e) {
+  if (e.target.tagName !== "A") {
+    return;
+  }
+  browser.runtime.sendMessage({ url: e.target.href });
+}
+
